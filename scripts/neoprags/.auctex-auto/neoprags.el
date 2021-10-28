d69781a0b7f967db2827d62a226bf605c78b21a3
LiquidZulu <liquidzulu@pm.me>
2021-10-28 16:50:01 +0100

    uhhhhhhhh

---
+++ scripts/neoprags/.auctex-auto/neoprags.el
@@ -0,0 +1,47 @@
+(TeX-add-style-hook
+ "neoprags"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:org537fe76"
+    "sec:org3fa1372"
+    "sec:org14af6b5"
+    "sec:org14c5852"
+    "sec:org2a8a915"
+    "sec:org74ba44e"
+    "sec:orgb70973b"
+    "sec:org95d4240"
+    "sec:org30097cd"
+    "sec:org741f1aa"
+    "sec:orgf8c1afe"
+    "sec:org1324cd6"))
+ :latex)
+
